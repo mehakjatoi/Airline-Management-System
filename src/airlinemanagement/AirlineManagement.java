e78c030a1ce2646d078ff3b26a33980f5f89143c
Mehak Jatoi <104321494+mehakjatoi@users.noreply.github.com>
2022-10-26 13:20:27 -0700

    final commit

--- src/airlinemanagement/AirlineManagement.java
+++ src/airlinemanagement/AirlineManagement.java
@@ -3,32 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package airlinemanagement;
-
+import javax.swing.JOptionPane;
 //import java.awt.Color;
 
 import java.awt.Color;
+ class Thread1 extends Thread{
+public void run(){
+try{
+Thread.sleep(100);
+}catch(Exception e){
+JOptionPane.showMessageDialog(null, e);
+}
+}
+}
 public class AirlineManagement {
-public static void main(String[] args) {
-   Splash S1=new Splash();
-   S1.setBounds(250,20,800,700);
-   Login login=new Login(); 
-   login.getContentPane().setBackground(Color.white);
-   login.setBounds(250,20,900,700);
-   //Management M1=new Management();
+
+    Splash S1=new Splash();
+   
+    public static void main(String[] args) {
+    Splash S1=new Splash();
     S1.setVisible(true);
+    Login log =new Login();
     try{
+         S1.setVisible(true); 
     for(int i=0;i<=100;i++){
-        Thread.sleep(60);
+      Thread1.sleep(100);
         S1.Loading.setValue(i);
     }
    }catch(Exception e){
-       e.getStackTrace();
+       e.getStackTrace(); 
    }
-   S1.setVisible(false);
-   login.setVisible(true);
-     S1.dispose();
-  // M1.setVisible(true);
-   
-    }
-    
+   S1.setVisible(false); 
+   log.setVisible(true); 
+}
 }
